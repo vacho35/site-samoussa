1ab9fbc5750f300b6567d91c5c16cf966af1e933
vacho35 <100503633+vacho35@users.noreply.github.com>
2023-03-07 19:42:20 +0100

    Add files via upload

--- mail.js
+++ mail.js
@@ -5,6 +5,16 @@ function sendMail() {
       blaze: document.getElementById("blaze").value,
       email: document.getElementById("email").value,
       number: document.getElementById("number").value,
+      boeufs: document.getElementById("samoussas_boeuf").value,
+      volaille: document.getElementById("samoussas_volaille").value,
+      legume: document.getElementById("samoussas_legume").value,
+      poisson: document.getElementById("samoussas_poisson").value,
+      chevre: document.getElementById("samoussas_chevre").value,
+      beignet_nature: document.getElementById("beignet_nature").value,
+      beignet_banane: document.getElementById("beignet_banane").value,
+      sauce_chocolat: document.getElementById("sauce_chocolat").value,
+      sauce_coco: document.getElementById("sauce_coco").value,
+      paiement: document.getElementById("paiement").value,
       message: document.getElementById("message").value,
     };
   
@@ -16,6 +26,16 @@ function sendMail() {
           document.getElementById("blaze").value = "";
           document.getElementById("email").value = "";
           document.getElementById("number").value = "";
+          document.getElementById("samoussas_boeuf").value = "";
+          document.getElementById("samoussas_volaille").value = "";
+          document.getElementById("samoussas_legume").value = "";
+          document.getElementById("samoussas_poisson").value = "";
+          document.getElementById("samoussas_chevre").value = "";
+          document.getElementById("beignet_nature").value = "";
+          document.getElementById("beignet_banane").value = "";
+          document.getElementById("sauce_chocolat").value = "";
+          document.getElementById("sauce_coco").value = "";
+          document.getElementById("paiement").value = "";
           document.getElementById("message").value = "";
           console.log(res);
           window.location.href='retour_commande.html';  
